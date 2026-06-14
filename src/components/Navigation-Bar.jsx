9dd5b622f6f14fa30cb3611abfa8d00b6307b01b
BJ008Creative <baishalinx@gmail.com>
2026-06-14 10:43:09 +0530

    first commit

---
+++ src/components/Navigation-Bar.jsx
@@ -0,0 +1,10 @@
+export function NavigationBar(){
+    return(
+        <div>
+            <h1>Aromatic-Archives</h1>
+            <p>An archive full of mouth watering dishes.</p>
+            <button >Upload</button>
+            
+        </div>
+    )
+}
