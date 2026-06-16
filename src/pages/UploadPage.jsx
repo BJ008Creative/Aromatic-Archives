6f238fc7994a1ae4211c81f34c84a3e6ce951bfc
BJ008Creative <baishalinx@gmail.com>
2026-06-16 21:43:48 +0530

    deleted upload sections

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,12 +1,52 @@
-import { NavigationBar } from "../components/Navigation-Bar";
-import { UploadSection } from "../components/UploadSection";
+import { useState } from "react";
+import { NavigationBar } from "../components/Navigation-Bar.jsx";
 
-export function UploadPage({recipes , setRecipes}){
-    return(
-         <>
-            <NavigationBar></NavigationBar>
-            <UploadSection recipes={recipes} ></UploadSection>
-        </> 
-    )
+export function UploadPage({ recipes, setRecipes }) {
+  const [recipeName, setRecipeName] = useState("");
+  const [author, setAuthor] = useState("");
+
+  function handleSubmit() {
+    const newRecipe = {
+      recipe: recipeName,
+      name: author,
+    };
+
+    setRecipes([...recipes, newRecipe]);
+
+    console.log("Recipe Added");
+  }
+
+  return (
+    <>
+        <NavigationBar></NavigationBar>
+    <div>
+      <h1>Upload Recipe</h1>
+
+      <input
+        type="text"
+        placeholder="Recipe Name"
+        value={recipeName}
+        onChange={(e) => setRecipeName(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+    </>
+    
+  );
 }
-
